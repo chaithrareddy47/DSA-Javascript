2817fbf876caca95d1a47c9ed555b9dce2b4107e
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-11-05 07:53:40 +0530

    Add files via upload

---
+++ 01-Programming-101/App.js
@@ -0,0 +1,46 @@
+// Programming 101
+console.log("Hello world") // printing something into console
+
+
+// 2 ways of creating vatiable
+const a = 10; // cannot be changed later in the program 
+
+let x = 20; // value can he changed later in the program
+
+// string concatination
+// adding number to a sting cwhich number concerted to string
+
+const fisrtName = "chaithra "
+const id = 47;
+
+console.log(fisrtName + id);
+console.log(typeof (fisrtName + id));
+
+
+// ARRAYS
+// array is a data structure it stores multiple values
+
+let arr = [10, 2, 3, 5, 2]
+console.log(arr[0]) // arrya is index based we find values throeugh indexes
+
+// array of strings 
+let arrStr = ['apple', 'banana', 'mango']
+console.log(arrStr);
+
+// array can contain diffent data type also
+
+let arrDiff = [10, 'apple', false, -10]
+console.log(typeof arrDiff);
+
+
+// objects
+//objects are created by using key value paies and curly braces
+
+let obj = {
+    a: 10,
+    b: 20,
+    
+}
+console.log(obj.a);
+
+
