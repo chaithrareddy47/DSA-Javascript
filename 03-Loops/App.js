fe997e60282e6d3bf3d965c204e825df1a9ee461
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-11-06 21:45:37 +0530

    Add files via upload

---
+++ 03-Loops/App.js
@@ -0,0 +1,39 @@
+// Loops
+
+// for (let i = 0; i < 4; i++){
+//     console.log(i + " Hello world");
+    
+// }
+
+// for (let i = 2; i < 9; i++){
+//     console.log(i + 1);
+    
+// }
+
+// for (let i = 5; i < 4; i++){
+//     console.log("Hello");
+    
+// }
+
+// // calling function inside loops
+
+// function greet(name) {
+//     console.log("Namste " + name);
+    
+// }
+
+// for (let i = 0; i < 4; i++){
+//     greet("chai")
+// }
+
+
+// Arrays with loops 
+let arr = [10, 2, 3, 4, 5, 7]
+
+// console.log(arr.length);
+// console.log(arr[0]);
+
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+    
+}
