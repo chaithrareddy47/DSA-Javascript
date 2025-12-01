7820150f807ce79685e6bc56e7ce9a19f2ac4a37
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-12-01 21:15:56 +0530

    Add files via upload

---
+++ Practial-problem/App.js
@@ -0,0 +1,48 @@
+// let arr = [2, 10, 4, 16, 5, 3, 2, 1]
+
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i]);
+        
+//     }
+// }
+
+// let arr = [4, 2, 10, 8]
+// function searchEle(arr, x) {
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] === x) {
+//             return i;
+            
+//         }
+//     }
+//     return -1;
+// }
+
+// let search = searchEle(arr, 3)
+// console.log(search);
+
+// let arr = [10, 2, 30, 50, 6]
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+    
+// }
+// let arr = [10, 20,30]
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++){
+//     sum = arr[i] + sum;
+
+    
+// }
+// console.log(sum);
+
+// let arr = [10, 120, 30, 50, 60]
+// let largest = 0;
+
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > largest) {
+//         largest=arr[i]
+//     }
+// }
+// console.log(largest);
+
+
