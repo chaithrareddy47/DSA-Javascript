50f14e47423675e27170fef533498a873d9dd9b4
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-12-02 21:16:48 +0530

    Add files via upload

--- Practial-problem/App.js
+++ Practial-problem/App.js
@@ -45,4 +45,49 @@
 // }
 // console.log(largest);
 
+// let arr = [-1, 2, 3, -4]
+// let positive = 0;
+// let negative = 0;
+
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > 0) {
+//         positive++;
+//     } else if (arr[i] < 0) {
+//         negative++;
+//     }
+// }
+
+// console.log(positive);
+// console.log(negative);
+
+// let arr = [90, 10, 25]
+// let largest = 0;
+// let secondLargest = 0;
+
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > largest) {
+//         secondLargest = largest;
+//         largest = arr[i];
+//     } else if (arr[i] > secondLargest) {
+//         secondLargest = arr[i];
+//     }
+// }
+
+// console.log(largest);
+// console.log("second larfest is " + secondLargest);
+
+
+let arr = [1, 2, 3, 4, 5]
+let start = 0;
+let end = arr.length - 1;
+
+while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+}
+
+console.log(arr);
 
